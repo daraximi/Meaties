ebcec66c37c1b6b62f7d22900c896854c608d3a8
Darasimi Oloyede <darasimioloyede@gmail.com>
2022-11-17 23:42:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# meaties_app
+
+Meaties Frontend Application Demo
+
+This is a demo of the Meaties Food Application.
---
+++ android/app/src/main/kotlin/com/meatiesdemo/android/meaties_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.meatiesdemo.android.meaties_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
